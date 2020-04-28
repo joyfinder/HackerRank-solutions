63d5f917e5f8e56a6decd099019f555edc23ff05
Ta-Liang Lu <scytl2@nottingham.edu.cn>
2020-04-28 11:51:15 +0800

    Day 0 submission
    Day 0 Hello World Solution uploaded

---
+++ 30 Days of Code/Day 0 Hello World/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+	public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in); 
+		
+		String inputString = scan.nextLine(); 
+		scan.close(); 
+      
+		// Print a string literal saying "Hello, World." to stdout.
+		System.out.println("Hello, World.");
+        System.out.println(inputString);
+      
+	    
+	}
+}
