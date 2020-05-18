3826cee0960aa359e9a9ef61b45cb269f292f221
Ta-Liang Lu <scytl2@nottingham.edu.cn>
2020-05-18 08:30:46 +0800

    Create Solution
    uploaded solutions

---
+++ SQL/Weather Observation Station 19/Weather Observation Station 19.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+*/
+SELECT round(sqrt(power(max(LAT_N) - min(LAT_N),2) + power(max(LONG_W) - min(LONG_W),2)),4)
+FROM STATION
+
---
+++ SQL/Weather Observation Station 20/Weather Observation Station 20.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+*/
+Select round(S.LAT_N,4) mediam 
+from station S 
+where (select count(Lat_N) from station where Lat_N < S.LAT_N ) = (select count(Lat_N) from station where Lat_N > S.LAT_N)
